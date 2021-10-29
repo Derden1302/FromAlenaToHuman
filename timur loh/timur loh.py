80b11fbf65a38913011dc296a0c14fd12207689a
kvasimba <dusa2530@gmail.com>
2021-10-29 19:57:56 +0300

    my first code

--- timur loh/timur loh.py
+++ timur loh/timur loh.py
@@ -1,2 +1,15 @@
-print ("Hello world") fff
-fdbeathjwjty7gin hijnuy678 tijkoy78 uijnouy789 m
+from timur_the_best.Best_of_me import square
+
+
+
+
+def f(a):
+    a=a%1000
+    a=int(a/100)
+    return(a)
+print(15+f(63957239457))
+
+c=364769
+square(f(c))
+d=square(f(c))
+print(d+d)
